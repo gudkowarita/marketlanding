e2b6b33afcea34e60ed575d4f573a134e79d045b
gudkowarita <42046544+gudkowarita@users.noreply.github.com>
2019-03-18 21:01:31 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,45 +1,54 @@
-
+window.addEventListener('DOMContentLoaded', () => {
     
-    const cartWrapper = document.querySelector('.cart__wrapper'),
-      cart = document.querySelector('.cart'),
+ const cartWrapper = document.querySelector('.cart__wrapper'),
+   cart = document.querySelector('.cart'),
       //кнопка закрытия корзины
-      cartClose = document.querySelector('.cart__close'),
+   close = document.querySelector('.cart__close'),
       //кнопка открытия корзины
-      cartOpen = document.querySelector('#cart'),
+   open = document.querySelector('#cart'),
       //общая стоимость товаров в корзине
-      cartTotalCost = document.querySelector('.cart__total > span'),
+    totalCost = document.querySelector('.cart__total > span'),
       //иконка корзины для анимации подтверждения добавления в корзину
-      aniCartIcon = document.querySelector('.confirm'),
+    confirm = document.querySelector('.confirm'),
       //бэйджик на иконке корзины для показа количества товаров в корзине
-      badge = document.querySelector('.nav__badge'),
+     badge = document.querySelector('.nav__badge'),
 
-      goodsButtons = document.querySelectorAll('.goods__btn'),
-      goods = document.querySelectorAll('.goods__item'),
+      goodsBtn = document.querySelectorAll('.goods__btn'),
+      products = document.querySelectorAll('.goods__item'),
       titles = document.querySelectorAll('.goods__title');
 
-    function showCart() {
+    function openCart() {
       //значение по-умолчанию display: none
       cart.style.display = 'block';
       //блокирует прокрутку страницы при открытой корзине
       document.body.overflow = 'hidden';
     }
 
-    function hideCart() {
+    function closeCart() {
       cart.style.display = 'none';
       //разблокировка прокрутки страницы
       document.body.overflow = '';
     }
 
-    cartOpen.addEventListener('click', showCart);
-    cartClose.addEventListener('click', hideCart);
+    open.addEventListener('click', openCart);
+    close.addEventListener('click', closeCart);
+goodsBtn.forEach(function(btn, i){
+	btn.addEventListener('click', () => {
+	    let item = products[i].cloneNode(true),
+		   trigger = item.querySelector('button'),
+		   removeBtn = document.createElement('div'),
+		   empty = cartWrapper.querySelector('.empty');
+    
+     trigger.remove();
+     removeBtn.classList.add('goods__item-remove');
+     removeBtn.innerHTML = '&times';
+     item.appendChild(removeBtn);
 
-    goodsButtons.forEach(function(btn, i){
-	btn.addEventListener('click', ()=> {
-	    let item = goods[i].cloneNode(true),
-		trigger = item.querySelector('button'),
-		removeBtn = document.createElement('div'),
-		empty = cartWrapper.querySelector('.empty');
-	});
+     cartWrapper.appendChild(item);
+     if (empty) {
+        empty.remove();
+     }
+   });
+  });
 });
 
-//1.31.02
