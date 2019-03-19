ea739de977362f8936865e0487326ab439fec6d4
gudkowarita <42046544+gudkowarita@users.noreply.github.com>
2019-03-19 21:05:47 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -32,23 +32,57 @@ window.addEventListener('DOMContentLoaded', () => {
 
     open.addEventListener('click', openCart);
     close.addEventListener('click', closeCart);
-goodsBtn.forEach(function(btn, i){
-	btn.addEventListener('click', () => {
+  goodsBtn.forEach(function(btn, i){  //добавление в корзину карточки товара
+  	btn.addEventListener('click', () => {
+      showConfirm();
+      calcGoods(1);
 	    let item = products[i].cloneNode(true),
-		   trigger = item.querySelector('button'),
-		   removeBtn = document.createElement('div'),
-		   empty = cartWrapper.querySelector('.empty');
+		    trigger = item.querySelector('button'),
+		    removeBtn = document.createElement('div'),
+		    empty = cartWrapper.querySelector('.empty');
     
-     trigger.remove();
-     removeBtn.classList.add('goods__item-remove');
-     removeBtn.innerHTML = '&times';
-     item.appendChild(removeBtn);
+      trigger.remove();
+      removeBtn.classList.add('goods__item-remove');
+      removeBtn.innerHTML = '&times';
+      item.appendChild(removeBtn);
 
-     cartWrapper.appendChild(item);
-     if (empty) {
+      cartWrapper.appendChild(item);
+        if (empty) {
         empty.remove();
-     }
-   });
+      }
+    });
   });
-});
 
+  titles.forEach(function(item){   //обрезка строк названий
+    if (item.textContent.length < 70 ) {
+     return;
+    } else {
+      const str = item.textContent.slice(0, 71) + '...';
+      item.textContent = str;
+    }
+  });
+
+  function showConfirm() {  //анимация при добавлении в корзину
+    confirm.style.display = 'block';
+    let counter = 100;  //счетчик
+    const id = setInterval(frame, 10);
+    function frame() {
+      if ( counter == 10) {
+        clearInterval(id);
+        confirm.style.display = 'none';
+      } else {
+      counter--;
+      confirm.style.opacity = '.' + counter;
+      confirm.style.transform = `translateY(-${counter}px)`;
+      }
+    }
+  }
+  
+  function calcGoods(i) {
+    const items = cartWrapper.querySelectorAll('.goods__item');
+    badge.textContent = i + items.length;
+  }
+  //setTimeOut(function, 1000)
+});  //function window onload
+
+//второе видео 1.01.23--чуть пораньше
